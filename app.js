4c1b8df4224b264ccd6cf220aaf6328c1662021f
molavi25 <alimolavi025@gmail.com>
2023-06-19 18:27:58 +0330

    added some pages and work with user POST request and user input data

--- app.js
+++ app.js
@@ -1,30 +1,98 @@
 const http = require("http");
+const fs = require("fs");
 
-const html = `
-<html>
-  <head>
-    <title>My First page</title>
-  </head>
-  <body>
-    <h1>Hello from Node.js server!</h1>
-  </body>
-</html>
+const index = `
+  <html>
+    <head>
+      <title>My First page</title>
+    </head>
+    <body>
+      <center>
+        <h1>Hello from Node.js server!</h1>
+      </center>
+    </body>
+  </html>
+`;
+
+const form = `
+  <html>
+    <head>
+      <title>signup</title>
+    </head>
+    <body>
+      <center>
+        <form action='result' method='POST' >
+          <input type="text" name='userName' />
+
+          <button type='submit'>send</button>
+        </form>
+      </center>
+    </body>
+  </html>
+`;
+
+const result = `
+  <html>
+    <head>
+      <title>result</title>
+    </head>
+    <body>
+      <center>
+        <h1>You logged in :)</h1>
+      </center>
+    </body>
+  </html>
+`;
+
+const notFound = `
+  <html>
+    <head>
+      <title>404</title>
+    </head>
+    <body>
+      <center>
+        <h1>PAGE NOT FOUND 404 :(</h1>
+      </center>
+    </body>
+  </html>
 `;
 
 const server = http.createServer(function (req, res) {
-  console.log(
-    `
-    URL: ${req.url}
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(index);
+    return res.end();
+  }
 
-    METHOD: ${req.method}
+  if (req.url === "/signup") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(form);
+    return res.end();
+  }
 
-  `,
-    "HEADERS: \n",
-    req.headers
-  );
+  if (req.url === "/result") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(result);
 
+    if (req.method === "POST") {
+      const body = [];
+      req.on("data", (chunk) => {
+        console.log("CHUNKS", chunk);
+        body.push(chunk);
+      });
+      req.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const userInfo = parsedBody.split("=")[1];
+
+        fs.writeFileSync("userInfo.txt", userInfo);
+      });
+
+      res.statusCode = 302;
+      return res.end();
+    }
+  }
   res.setHeader("Content-Type", "text/html");
-  res.write(html);
+  res.write(notFound);
   res.end();
 });
 
