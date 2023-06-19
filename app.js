d6dfd19648a9e52ef44d9d3dbfe5ba0529c91f64
molavi25 <alimolavi025@gmail.com>
2023-06-19 16:43:38 +0330

    res object

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const http = require("http");
 
+const html = `
+<html>
+  <head>
+    <title>My First page</title>
+  </head>
+  <body>
+    <h1>Hello from Node.js server!</h1>
+  </body>
+</html>
+`;
+
 const server = http.createServer(function (req, res) {
   console.log(
     `
@@ -11,6 +22,10 @@ const server = http.createServer(function (req, res) {
     "HEADERS: \n",
     req.headers
   );
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(html);
+  res.end();
 });
 
 server.listen(4000);
