5d514ea2c372966fd6ae57432aa69c78afc492d8
molavi25 <alimolavi025@gmail.com>
2023-08-05 19:22:10 +0330

    work with debuger

--- routes.js
+++ routes.js
@@ -1,74 +1,18 @@
 import fs from "fs";
 
-const index = `
-  <html>
-    <head>
-      <title>My First page</title>
-    </head>
-    <body>
-      <center>
-        <h1>Hello from Node.js server!</h1>
-      </center>
-    </body>
-  </html>
-`;
-
-const form = `
-  <html>
-    <head>
-      <title>signup</title>
-    </head>
-    <body>
-      <center>
-        <form action='result' method='POST' >
-          <input type="text" name='userName' />
-
-          <button type='submit'>send</button>
-        </form>
-      </center>
-    </body>
-  </html>
-`;
-
-const result = `
-  <html>
-    <head>
-      <title>result</title>
-    </head>
-    <body>
-      <center>
-        <h1>You logged in :)</h1>
-      </center>
-    </body>
-  </html>
-`;
-
-const notFound = `
-  <html>
-    <head>
-      <title>404</title>
-    </head>
-    <body>
-      <center>
-        <h1>PAGE NOT FOUND 404 :(</h1>
-      </center>
-    </body>
-  </html>
-`;
-
 const requestHandler = (req, res) => {
   const {url, method} = req;
   //~ HOME /////////////////////////////////////
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
-    res.write(index);
+    res.write(Pages.index);
     return res.end();
   }
 
   //~ SIGNUP //////////////////////////////////
   if (url === "/signup") {
     res.setHeader("Content-Type", "text/html");
-    res.write(form);
+    res.write(Pages.form);
     return res.end();
   }
 
@@ -87,7 +31,7 @@ const requestHandler = (req, res) => {
 
       fs.writeFile("userInfo.txt", userInfo, (err) => {
         res.setHeader("Content-Type", "text/html");
-        res.write(result);
+        res.write(Pages.result);
         res.statusCode = 302;
         return res.end();
       });
@@ -95,8 +39,65 @@ const requestHandler = (req, res) => {
   }
   //~ 404 /////////////////////////////////////
   res.setHeader("Content-Type", "text/html");
-  res.write(notFound);
+  res.write(Pages.notFound);
   res.end();
 };
 
 export default requestHandler;
+
+var Pages = {
+  index: `
+  <html>
+    <head>
+      <title>My First page</title>
+    </head>
+    <body>
+      <center>
+        <h1>Hello from Node.js server!</h1>
+      </center>
+    </body>
+  </html>
+`,
+  form: `
+  <html>
+    <head>
+      <title>signup</title>
+    </head>
+    <body>
+      <center>
+        <form action='result' method='POST' >
+          <input type="text" name='userName' />
+
+          <button type='submit'>send</button>
+        </form>
+      </center>
+    </body>
+  </html>
+`,
+
+  result: `
+  <html>
+    <head>
+      <title>result</title>
+    </head>
+    <body>
+      <center>
+        <h1>You logged in :)</h1>
+      </center>
+    </body>
+  </html>
+`,
+
+  notFound: `
+  <html>
+    <head>
+      <title>404</title>
+    </head>
+    <body>
+      <center>
+        <h1>PAGE NOT FOUND 404 :(</h1>
+      </center>
+    </body>
+  </html>
+`,
+};
